f1ff7b4915eb614b75402b2b41d0cf9bc9b7fb32
Rajat Mittal <rjmittal97@gmail.com>
2018-09-30 12:00:23 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:first_flutter/category.dart';
+import 'package:first_flutter/category_route.dart';
+
+const _categoryName = "Cake";
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+void main(){
+  runApp(UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     title: "Unit Converter",
+     home: CategoryRoute(),
+   );
+  }
+}
+
